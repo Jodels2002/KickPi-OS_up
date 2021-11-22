19da1ad864e7a9f6e45a805021e232830e698969
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-11-22 19:26:07 +0000

    Update boot.sh

--- scripts/boot.sh
+++ scripts/boot.sh
@@ -1,2 +1,17 @@
+
+if [ -d /media/pi/rootfs/home/pi/pimiga/disks/ ]; then
+ 
+ cd /OLED
+python3 pimiga.py
+xfce4-terminal -e m
+
+
+ 
+ fi
+
+
+cd /OLED
+python3 boot.py
+
 cd /OLED
 python3 boot.py

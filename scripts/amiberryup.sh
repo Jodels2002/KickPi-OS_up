e1e7722d3d4cd3fbfd1458bd82a7f2e1eafdd62f
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-11-22 20:58:51 +0000

    Update amiberryup.sh

--- scripts/amiberryup.sh
+++ scripts/amiberryup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Install KickPi-OS
 # B.Titze 2021
+  sudo apt-get -y install libsdl2-2.0-0 libsdl2-ttf-2.0-0 libsdl2-image-2.0-0 flac mpg123 libmpeg2-4
+  sudo apt-get -y install libsdl2-dev libsdl2-ttf-dev libsdl2-image-dev libflac-dev libmpg123-dev libpng-dev libmpeg2-4-dev libraspberrypi-dev
 
 
 Update_Amiberry.sh

f7bcb51ed6a471bd9034d118cbfeb45fff8bbc72
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-11-24 05:20:50 +0000

    Add files via upload

---
+++ scripts/AFimport.sh
@@ -0,0 +1,11 @@
+
+f [ -f /home/pi/Desktop/AmigaForever9Plus.msi]; then
+
+sudo apt install -y msitools
+cd /home/pi/Templates/
+msiextract /home/pi/Desktop/AmigaForever9Plus.msi
+
+cp -r -f "/home/pi/Templates/Program Files/Cloanto/Amiga Forever/Shared/"* /home/pi/Amiga/
+mv /home/pi/Amiga/rom/* /home/pi/Amiga/kickstarts/
+rm -d /home/pi/Amiga/rom/
+fi

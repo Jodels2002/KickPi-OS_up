45efda404c72d757993347592d689bb91f100f26
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-11-26 09:09:50 +0100

    Update firstboot.sh

--- scripts/firstboot.sh
+++ scripts/firstboot.sh
@@ -15,31 +15,6 @@ sudo raspi-config nonint get_ssh
 sudo raspi-config nonint do_i2c 0
 sudo raspi-config nonint do_expand_rootfs
 
-wget -q --spider http://google.com
-
- if [ $? -eq 0 ]; then
-
-
-
-sudo rm -rf /home/$USER/KickPi-OS/
-
-
-      cd
-      git clone --depth=1 https://github.com/Jodels2002/KickPi-OS_up.git
-      sudo mv /home/$USER/KickPi-OS_up /home/$USER/KickPi-OS  
-      sudo chmod -R 777 KickPi-OS
-      
-      
-      sudo cp -f -R /home/$USER/KickPi-OS/scripts/* /usr/local/bin
-      sudo cp -f -R /home/$USER/KickPi-OS/ /opt
-      
-      sudo chmod -R 777 /usr/local/bin
-      sudo chmod -R 777 /opt/KickPi-OS
-      sudo chmod -R 777 /home/pi/Desktop/
-      /home/$USER/KickPi-OS/scripts/pikickme.sh
- else
-    whiptail --msgbox " Sorry, you must first connect to internet :-)" 20 50 1
- fi
        sudo rm -rf /home/$USER/.bashrc
        cp -rf /opt/KickPi-OS/scripts/.bashrc /home/$USER/.bashrc
        sudo chmod -R 777 /home/$USER/.bashrc

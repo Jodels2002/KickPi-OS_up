87da23680048f45d0a8cbb8a784b7fcdb4d9bf0e
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-12-01 11:08:04 +0000

    Update AFimport.sh

--- scripts/AFimport.sh
+++ scripts/AFimport.sh
@@ -1,5 +1,5 @@
 
-f [ -f /home/pi/Desktop/AmigaForever9Plus.msi]; then
+if [ -f /home/pi/Desktop/AmigaForever9Plus.msi ]; then
 
 sudo apt install -y msitools
 cd /home/pi/Templates/
@@ -8,4 +8,9 @@ msiextract /home/pi/Desktop/AmigaForever9Plus.msi
 cp -r -f "/home/pi/Templates/Program Files/Cloanto/Amiga Forever/Shared/"* /home/pi/Amiga/
 mv /home/pi/Amiga/rom/* /home/pi/Amiga/kickstarts/
 rm -d /home/pi/Amiga/rom/
+
+ cd
+sudo chmod -R 777 Amiga
+else
+clear
 fi

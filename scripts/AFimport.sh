da4e5d4bdb752f7b2dbafc2488f989243c494620
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-12-02 06:55:54 +0100

    Update AFimport.sh

--- scripts/AFimport.sh
+++ scripts/AFimport.sh
@@ -11,6 +11,51 @@ rm -d /home/pi/Amiga/rom/
 
  cd
 sudo chmod -R 777 Amiga
+
+if [ ! -d /home/pi/Amiga/dir/WB ]; then
+      sudo python3 -m pip install -U pip
+      sudo python3 -m pip install -U setuptools
+      sudo pip install amitools  
+      
+      cd /home/pi/Amiga/adf/
+      mkdir /home/pi/Amiga/dir/WB
+      #xdftool amiga-os-300-workbench.adf unpack /home/pi/Amiga/dir/WB
+      xdftool amiga-os-310-workbench.adf unpack /home/pi/Amiga/dir/WB
+      xdftool amiga-os-310-extras.adf unpack /home/pi/Amiga/dir/WB
+      xdftool amiga-os-310-fonts.adf unpack /home/pi/Amiga/dir/WB
+      xdftool amiga-os-310-locale.adf unpack /home/pi/Amiga/dir/WB
+      xdftool amiga-os-310-storage.adf unpack /home/pi/Amiga/dir/WB
+      xdftool amiga-os-310-install.adf unpack /home/pi/Amiga/dir/WB
+     fi 
+       mkdir /home/$USER/Amiga/conf/ 
+               
+      if [ ! -f /home/$USER/Amiga/Install/ClassicWB_UAE_v28.zip ]; then
+      clear
+      toilet "KickPi-OS" --metal
+      toilet "full" --metal
+      
+      mkdir /home/pi/Amiga/dir/Work
+      mkdir /home/pi/Amiga/Install/
+      cd /home/pi/Amiga/Install
+      
+      clear
+      wget http://download.abime.net/classicwb/ClassicWB_UAE_v28.zip
+      clear
+      unzip -u ./ClassicWB_UAE_v28.zip
+      clear
+      mv  "/home/pi/Amiga/Install/ClassicWB_UAE_v28/Hard Disk/Software/" /home/pi/Amiga/dir/Work
+      
+        else 
+      echo " "
+      
+      fi
+
+
+
+
+
+
+
 else
 clear
 fi

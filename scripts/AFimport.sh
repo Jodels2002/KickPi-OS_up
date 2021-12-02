0991a60c9026e486105f8966ce23c77f74355082
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-12-02 08:08:07 +0000

    Update AFimport.sh

--- scripts/AFimport.sh
+++ scripts/AFimport.sh
@@ -74,7 +74,7 @@ if [ ! -d /home/pi/Amiga/Install/WB ]; then
             mkdir /home/pi/Amiga/dir/System_ADVSP
       cd "/home/pi/Amiga/Install/ClassicWB_UAE_v28/Hard Disk/"
       xdftool System_ADVSP.hdf unpack /home/pi/Amiga/dir/System_ADVSP
-      
+      mkdir /home/pi/Amiga/dir/System_ADVSP/System/T/Workbench31
       cp -rf /home/pi/Amiga/dir/WB/* /home/pi/Amiga/dir/System_ADVSP/System/T/Workbench31
       cp -rf /opt/KickPi-OS/Amiga/ClassicWB/Startup-Sequence /home/pi/Amiga/dir/System_ADVSP/System/S/
       cp -rf /opt/KickPi-OS/Amiga/ClassicWB/Assign-Startup /home/pi/Amiga/dir/System_ADVSP/System/S/
@@ -93,6 +93,7 @@ if [ ! -d /home/pi/Amiga/Install/WB ]; then
       mkdir /home/pi/Amiga/dir/System_P96
       cd "/home/pi/Amiga/Install/ClassicWB_UAE_v28/Hard Disk/"
       xdftool System_P96.hdf unpack /home/pi/Amiga/dir/System_P96
+      mkdir /home/pi/Amiga/dir/System_P96/System/T/Workbench31
       cp -rf /home/pi/Amiga/dir/WB/* /home/pi/Amiga/dir/System_P96/System/T/Workbench31
       cp -rf /opt/KickPi-OS/Amiga/ClassicWB/Startup-Sequence /home/pi/Amiga/dir/System_P96/System/S/
       cp -rf /opt/KickPi-OS/Amiga/ClassicWB/User-Startup /home/pi/Amiga/dir/System_P96/System/S/

c4ddb78e3689147916ebc8fb142eb5517169d082
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-12-06 08:36:26 +0100

    Update Pimiga-imp.sh

--- scripts/Pimiga-imp.sh
+++ scripts/Pimiga-imp.sh
@@ -90,6 +90,70 @@ if  xset q &>/dev/null; then
       
 fi
 
+if [ -d /media/pi/rootfs/home/pi/pimiga/disks/AGSYSTEM/ ]; then
+
+
+      sudo mkdir /home/pi/pimiga2/
+      sudo mkdir /home/pi/pimiga2/disks/
+      clear
+      echo " "
+      echo " "
+      echo " Pimiga 1.5 MF found"
+      echo "      ..copy files"
+
+      sudo rsync -av --update /media/pi/rootfs/home/pi/pimiga/disks/ /home/pi/pimiga2/disks/
+      Pimiga.sh 
+      # Import Rom=s from WHDLoad
+      sudo cp  -rf  /media/pi/rootfs/home/pi/pimiga/disks/System/Devs/Kickstarts/kick33180.A500 /home/pi/Amiga/kickstarts/A500.rom
+      sudo cp  -rf  /media/pi/rootfs/home/pi/pimiga/disks/System/Devs/Kickstarts/kick40063.A600 /home/pi/Amiga/kickstarts/A600.rom
+      sudo cp  -rf  /media/pi/rootfs/home/pi/pimiga/disks/System/Devs/Kickstarts/kick40068.1200 /home/pi/Amiga/kickstarts/A1200.rom
+      sudo cp  -rf  /media/pi/rootfs/home/pi/pimiga/disks/System/Devs/Kickstarts/kick40068.4000 /home/pi/Amiga/kickstarts/A4000.rom
+      Pimiga.sh 
+      sudo cp  -rf  /media/pi/rootfs/home/pi/pimiga/disks/System/Devs/Kickstarts/*.rom /home/pi/Amiga/kickstarts/
+      sudo cp  -rf  /media/pi/rootfs/home/pi/pimiga/disks/System/Devs/Kickstarts/*.key /home/pi/Amiga/conf/
+      sudo cp  -rf  /opt/KickPi-OS/config/Pimiga2.desktop /home/pi/Desktop/
+      sudo cp  -rf  /opt/KickPi-OS/config/config/Pimiga2.uae /home/pi/Amiga/conf/
+
+      sudo chmod -R 777 /home/pi/Desktop/
+
+      #sudo rsync --info=progress2 -auvz /home/pi/pimiga/disks/System/Emulators/Mame0.34/roms/ /home/pi/RetroPie/roms/arcade/
+      Pimiga.sh 
+      #sudo rsync --info=progress2 -auvz /home/pi/pimiga/disks/System/Emulators/GnGeo/roms/ /home/pi/RetroPie/roms/neogeo/
+      #sudo cp -R -f -v /home/pi/pimiga/disks/System/Emulators/PocketSNES/roms/ /home/pi/RetroPie/roms/mastersystem/
+      #sudo chmod -R 777 /home/pi/Desktop/ /home/pi/RetroPie/roms/
+
+      Pimiga.sh 
+
+ 
+   
+      clear
+      echo " "
+      echo " "
+      echo " Copy PM 1.5 finished "
+      echo "      ..configure files ..."
+      
+      sudo chmod -R 777 /home/pi/pimiga15/
+      sudo chmod -R 777 /home/pi/Amiga/kickstarts/
+
+      echo " "
+      echo " "
+      echo "      ready ...fire up Pimiga"
+      cd /home/pi/Amiga/  
+ 
+ 
+      ./amiberry --config conf/Pimiga2.uae -G     
+      
+     
+      
+      else
+      clear
+      echo " "
+      echo " "
+      echo " copy finished "
+      echo "      ..configure files ..."
+      fi 
+
+
 if [ -d /media/pi/rootfs/home/pi/pimiga/ ]; then
       cd
       clear

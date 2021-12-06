9e2d747f88df877430516195819fba056d8540a0
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-12-06 09:42:54 +0100

    Update Pimiga-imp.sh

--- scripts/Pimiga-imp.sh
+++ scripts/Pimiga-imp.sh
@@ -111,7 +111,7 @@ if [ -d /media/pi/rootfs/home/pi/pimiga/disks/AGSYSTEM/ ]; then
       Pimiga.sh 
       sudo cp  -rf  /media/pi/rootfs/home/pi/pimiga/disks/System/Devs/Kickstarts/*.rom /home/pi/Amiga/kickstarts/
       sudo cp  -rf  /media/pi/rootfs/home/pi/pimiga/disks/System/Devs/Kickstarts/*.key /home/pi/Amiga/conf/
-      sudo cp  -rf  /opt/KickPi-OS/config/Pimiga2.desktop /home/pi/Desktop/
+      sudo cp  -rf  /opt/KickPi-OS/config/Pimiga2.0.desktop /home/pi/Desktop/
       sudo cp  -rf  /opt/KickPi-OS/config/config/Pimiga2.uae /home/pi/Amiga/conf/
 
       sudo chmod -R 777 /home/pi/Desktop/

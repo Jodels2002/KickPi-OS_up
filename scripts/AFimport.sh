ccf949abb30971b87eaf90993a6b4eaf30ad6e8e
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-12-09 07:56:52 +0100

    Update AFimport.sh

--- scripts/AFimport.sh
+++ scripts/AFimport.sh
@@ -1,12 +1,3 @@
-git clone https://github.com/ptitSeb/box64
-cd box64
-mkdir build; cd build; cmake .. -DSD845=1 -DCMAKE_BUILD_TYPE=RelWithDebInfo
-make -j4
-sudo make install
-sudo systemctl restart systemd-binfmt
-
-
-wget -qO- https://raw.githubusercontent.com/Botspot/pi-apps/master/install | bash
 
 
 if [ -f /home/pi/Desktop/AmigaForever9Plus.msi ]; then

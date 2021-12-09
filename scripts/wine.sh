7f55b8aec28bf5b4e84c64abdf4bcc61b0d8fa75
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-12-09 10:29:22 +0100

    Update wine.sh

--- scripts/wine.sh
+++ scripts/wine.sh
@@ -9,3 +9,14 @@ sudo systemctl restart systemd-binfmt
 #wget -qO- https://raw.githubusercontent.com/Botspot/pi-apps/master/install | bash
 
 diff -rq Projekt_alt Projekt_neu >unterschiede.txt 2>&1
+
+
+FREE=`df -k --output=avail "$PWD" | tail -n1`   # df -k not df -h
+if [[ $FREE -lt 67108 ]]; then               # 64G = 10*1024*1024k
+    
+    eche "Less than 164GB free!"
+    
+    
+    else 
+    df --output=avail -h "$PWD" | sed '1d;s/[^0-9]//g'
+fi;

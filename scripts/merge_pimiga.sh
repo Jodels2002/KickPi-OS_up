95fb45967807dc9791ccd3367c85d12ac3326555
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-12-09 11:09:01 +0100

    Update merge_pimiga.sh

--- scripts/merge_pimiga.sh
+++ scripts/merge_pimiga.sh
@@ -7,7 +7,7 @@ if [[ $FREE -lt 67108 ]]; then               # 64G = 10*1024*1024k
     
     else 
     df --output=avail -h "$PWD" | sed '1d;s/[^0-9]//g'
-    sudo rsync -av --ignore-existing /home/pi/pimiga15/ /home/pi/pimiga15/ --progress >> Pimiga_merge_log.txt
+    sudo rsync -av --ignore-existing /home/pi/pimiga15/ /home/pi/pimiga2/ --progress >> Pimiga_merge_log.txt
     
 fi;
 

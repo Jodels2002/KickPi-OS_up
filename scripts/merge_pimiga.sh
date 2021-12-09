33ead9f495cb9c77fb4201e2153e1c2384c5823d
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-12-09 10:52:49 +0100

    Create merge_pimiga.sh

---
+++ scripts/merge_pimiga.sh
@@ -0,0 +1,13 @@
+
+FREE=`df -k --output=avail "$PWD" | tail -n1`   # df -k not df -h
+if [[ $FREE -lt 67108 ]]; then               # 64G = 10*1024*1024k
+    
+    eche "Less than 64GB free!"
+    sudo rsync -av --update /home/pi/pimiga15/ /home/pi/pimiga15/ --progress >> Pimiga_merge_log.txt
+    
+    else 
+    df --output=avail -h "$PWD" | sed '1d;s/[^0-9]//g'
+fi;
+
+
+

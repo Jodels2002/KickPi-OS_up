cc7cb442a32aee3155d85919e1d83cadc56d2a8a
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-12-12 11:35:59 +0100

    Update amiberryup.sh

--- scripts/amiberryup.sh
+++ scripts/amiberryup.sh
@@ -72,5 +72,5 @@ fi
 
 sudo chmod -R 777 /home/$USER/amiberry
 cp -rf /home/$USER/amiberry/amiberry  /home/pi/Amiga/amiberry_dev
-cp -rf /home/$USER/amiberry/amiberry  /home/pi/Amiga/amiberry
+cp -rf /home/$USER/amiberry/amiberry  /home/pi/Amiga/amiberry_sdl
 

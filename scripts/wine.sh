7fc51baff4172030466db30aa67d497720c9ccfa
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-12-13 15:40:33 +0000

    Update wine.sh

--- scripts/wine.sh
+++ scripts/wine.sh
@@ -1,3 +1,15 @@
+#Box86
+git clone https://github.com/ptitSeb/box86
+sudo dpkg --add-architecture armhf
+cd ~/box86
+mkdir build
+cd build
+cmake .. -DRPI4ARM64=1 -DCMAKE_BUILD_TYPE=RelWithDebInfo
+make -j$(nproc)
+
+
+
+#Box64
 git clone https://github.com/ptitSeb/box64
 cd box64
 mkdir build; cd build; cmake .. -DSD845=1 -DCMAKE_BUILD_TYPE=RelWithDebInfo

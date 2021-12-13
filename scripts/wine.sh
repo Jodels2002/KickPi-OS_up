ee24f387fb4db72efecf0d13a72b10a0f7355266
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-12-13 11:45:05 +0000

    Update wine.sh

--- scripts/wine.sh
+++ scripts/wine.sh
@@ -5,18 +5,9 @@ make -j4
 sudo make install
 sudo systemctl restart systemd-binfmt
 
+msiextract /home/pi/Desktop/AmigaForever9Plus.msi
 
 #wget -qO- https://raw.githubusercontent.com/Botspot/pi-apps/master/install | bash
 
-diff -rq /home/pi/pimiga15/ /home/pi/pimiga2/ >unterschiede.txt 2>&1
+#diff -rq /home/pi/pimiga15/ /home/pi/pimiga2/ >unterschiede.txt 2>&1
 
-
-FREE=`df -k --output=avail "$PWD" | tail -n1`   # df -k not df -h
-if [[ $FREE -lt 67108 ]]; then               # 64G = 10*1024*1024k
-    
-    eche "Less than 64GB free!"
-    
-    
-    else 
-    df --output=avail -h "$PWD" | sed '1d;s/[^0-9]//g'
-fi;

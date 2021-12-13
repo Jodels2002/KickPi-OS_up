d264dae5449f1b97c383a5107afd36b2ee87a150
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-12-13 07:41:34 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -15,7 +15,9 @@
 #  # disable_overscan=1 -> Check
 #      hdmi_group=1
 #     hdmi_mode=16
-
+# Keyboard fixed
+# Importer for PM1 & 2
+# https://drive.google.com/file/d/1-V6zfCQo8KlHfqu86Yd27qM-gPY9i2DY/view?usp=sharing Amilator
 
 
 

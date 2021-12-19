ac752ff975412211a1573f4b2325bf95e4c03702
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-12-19 06:27:13 +0000

    Update kickmenu.sh

--- scripts/kickmenu.sh
+++ scripts/kickmenu.sh
@@ -62,7 +62,7 @@ case $CHOICE in
             toilet "KickPi-OS" --metal
             sudo rm /etc/profile.d/kick.sh
             sudo raspi-config nonint do_boot_behaviour B4
-            
+            sudo cp -rf /opt/KickPi-OS/config/splash/Booting.png /etc/systemd/system/Booting.png
             ;;
         
                    
@@ -70,6 +70,7 @@ case $CHOICE in
             clear
             toilet "Amiberry" --metal
              sudo cp -rf /opt/KickPi-OS/config/kick.sh /etc/profile.d/kick.sh
+             sudo cp -rf /opt/KickPi-OS/config/splash/KS31.png /etc/systemd/system/Booting.png
              sudo chmod -R 777 /etc/profile.d/kick.sh
              sudo raspi-config nonint do_boot_behaviour B2
              echo " "
@@ -87,6 +88,7 @@ case $CHOICE in
             clear
             toilet "KickPi-OS" --metal
             sudo rm /etc/profile.d/kick.sh
+            sudo cp -rf /opt/KickPi-OS/config/splash/Amiga.png /etc/systemd/system/Booting.png
             sudo raspi-config nonint do_boot_behaviour B2
             echo " "
             echo " "
@@ -181,9 +183,30 @@ case $CHOICE in
             
             
 esac
+BLACK='\033[0;39m'
+BLUE='\033[1;34m'
+GREEN='\033[1;32m'
+RED='\033[1;31m'
 
-
+cd
 clear
 toilet "KickPi-OS" --metal
+echo -e "$BLUE KickPI-OS ROM Operating System and Libraries" 
+echo " Version V2.0 2020-2021 KickPi-OS "
+echo " No Rights Reserved.  "
+echo -e "$BLACK "
+echo " Type 'd' to boot into Kick-OS Workbench"
+echo ""
+echo " 1.>  "
+echo -e " 1.>  ( a ) Start Amiga                   *Amiberry*" 
+echo " 1.>  ( d ) KickPi Desktop                          " 
+echo -e " 1.>  ( e ) Emulationstation              *RetroPie*" 
+echo " 1.>  ( r ) Emulationstation Setup                  "
+echo -e " 1.>  ( u ) KickPi-OS Update                        "
+echo " 1.>  ( m ) KickPi-OS Config                        " 
+echo -e " 1.>  ( c ) Raspi-Config                            "
+echo " 1.>  ( s ) Shutdown                                "  
+echo ""
+
 
 

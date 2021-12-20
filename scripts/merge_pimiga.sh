3360ba00dd5cf5c10bad14ec68e4a53de598b28a
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-12-20 17:08:19 +0000

    Update merge_pimiga.sh

--- scripts/merge_pimiga.sh
+++ scripts/merge_pimiga.sh
@@ -5,17 +5,14 @@ diff -u pimiga2.txt pimiga15.txt | sort -k 2 > diff.txt
 
 
 FREE=`df -k --output=avail "$PWD" | tail -n1`   # df -k not df -h
-if [[ $FREE -lt 67108 ]]; then               # 64G = 10*1024*1024k
+if [[ $FREE -lt 33554432 ]]; then               # 32G = 10*1024*1024k
     
-    echo "Less than 64GB free!"
+    echo "Less than 32GB free!"
     
     
     else 
     #df --output=avail -h "$PWD" | sed '1d;s/[^0-9]//g'
     #sudo rsync -av --ignore-existing /home/pi/pimiga15/ /home/pi/pimiga2/ 
     # sudo rsync -av --ignore-existing /home/pi/pimiga15/ /home/pi/pimiga2/ --progress >> Pimiga_merge_log.txt
-    
+    echo "More than 32GB free!"
 fi;
-
-
-

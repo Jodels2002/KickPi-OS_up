d9986fec115e6196085a590c5e2365a0f2b980d3
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-12-26 08:51:04 +0000

    Update check_status.sh

--- scripts/check_status.sh
+++ scripts/check_status.sh
@@ -3,24 +3,8 @@ wget -q --spider http://google.com
  if [ $? -eq 0 ]; then
 
 
-
-      sudo rm -rf /home/$USER/KickPi-OS/
-
-
-      cd
-      git clone --depth=1 https://github.com/Jodels2002/KickPi-OS_up.git
-      sudo mv /home/$USER/KickPi-OS_up /home/$USER/KickPi-OS  
-      sudo chmod -R 777 KickPi-OS
-      
-      
-      sudo cp -f -R /home/$USER/KickPi-OS/scripts/* /usr/local/bin
-      sudo rm -rf /opt/KickPi-OS
-      sudo cp -f -R /home/$USER/KickPi-OS/ /opt
-      
-      sudo chmod -R 777 /usr/local/bin
-      sudo chmod -R 777 /opt/KickPi-OS
-      sudo chmod -R 777 /home/pi/Desktop/
-      /home/$USER/KickPi-OS/scripts/pikickme.sh
+ cd
+     
  
  else
     whiptail --msgbox " Sorry, you must first connect to internet ..." 20 50 1

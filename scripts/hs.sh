418b1707cc492113e6d6e6abfb288efdb40ffd61
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2022-01-03 09:38:03 +0100

    Create hs.sh

---
+++ scripts/hs.sh
@@ -0,0 +1,5 @@
+#https://github-wiki-see.page/m/henrikstengaard/hstwb-installer/wiki/HstWB-Installer-for-Raspberry-Pi-OS-Lite
+
+
+git clone https://www.github.com/henrikstengaard/hstwb-installer.git
+./hstwb-installer/launcher/raspberry-pi/install.sh

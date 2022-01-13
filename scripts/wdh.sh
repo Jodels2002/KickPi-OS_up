cb460203014c769a3c0dedafa8abe2c382f316fd
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2022-01-13 08:30:00 +0000

    Update wdh.sh

--- scripts/wdh.sh
+++ scripts/wdh.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+!/bin/bash
 # Install KickPi-OS
 # B.Titze 2020
 
@@ -30,14 +30,14 @@ cd
       echo -e "$GREY " 
 
 find . -type f -name '*.slave' | sed -r 's|/[^/]+$||' |sort |uniq > ~/tmp.txt
-echo "$(grep -v "./Amiga/dir/Games/WHDLoad_Games/" ~/tmp.txt)" > ~/G.txt
+echo "$(grep -v "./Amiga/dir/Work/WHDLoad_Games/" ~/tmp.txt)" > ~/G.txt
 awk '{print $0"/"}' ~/G.txt > ~/Ga.txt
 awk '{ print "\""$0"\""}' ~/Ga.txt > ~/Game.txt
 
-if [ ! -d ~/Amiga/dir/Games/WHDLoad_Games/ ]; then
-          mkdir ~/Amiga/dir/Games/
-          mkdir ~/Amiga/dir/Games/WHDLoad_Games/
-          mkdir ~/Amiga/dir/Games/WHDLoad_Games/OCS/       
+if [ ! -d ~/Amiga/dir/Work/WHDLoad_Games/ ]; then
+          mkdir ~/Amiga/dir/Work/
+          mkdir ~/Amiga/dir/Work/WHDLoad_Games/
+          mkdir ~/Amiga/dir/Work/WHDLoad_Games/OCS/       
 fi
       
 ##
@@ -46,10 +46,10 @@ declare -a arr=( "Demo" "Demos" "CDTV" "De" "Fr" "Es" "It" "Gr" "AGA" "CD32" "OC
 for i in "${arr[@]}"
 do
 
-   Dest="~/Amiga/dir/Games/WHDLoad_Games/"$i
-    if [ ! -d ~/Amiga/dir/Games/WHDLoad_Games/"$i" ]; then
+   Dest="~/Amiga/dir/Work/WHDLoad_Games/"$i
+    if [ ! -d ~/Amiga/dir/Work/WHDLoad_Games/"$i" ]; then
     
-        mkdir ~/Amiga/dir/Games/WHDLoad_Games/"$i"       
+        mkdir ~/Amiga/dir/Work/WHDLoad_Games/"$i"       
    fi
    
    echo "Creating New WHDLoad folder "$Dest" "
@@ -67,7 +67,7 @@ do
 done
 
 # Cleanup NTSC
-cd /home/pi/Amiga/dir/Games/WHDLoad_Games/
+cd /home/pi/Amiga/dir/Work/WHDLoad_Games/
 
 find . -type f -name 'igame.iff' | sed -r 's|/[^/]+$||' |sort |uniq > ~/G.txt
 awk '{print $0"/"}' ~/G.txt > ~/Ga.txt
@@ -80,7 +80,7 @@ declare -a arr=( "NTSC" "PAL" )
 
 for i in "${arr[@]}"
 do
-   Dest="~/Amiga/dir/Games/WHDLoad_Games/"$i
+   Dest="~/Amiga/dir/Work/WHDLoad_Games/"$i
 
    echo "Creating New WHDLoad folder "$Dest" "
    grep "$i/" ~/Gamecp.txt > ~/tmp.txt
@@ -97,40 +97,40 @@ done
 
 fi
 # Cleanup Games
-if [ -d /home/pi/Amiga/dir/Games/WHDLoad_Games/ ]; then
-   mv  ~/Amiga/dir/Games/WHDLoad_Games/Games/* ~/Amiga/dir/Games/WHDLoad_Games/OCS/
-   #sudo rm -rf ~/Amiga/dir/Games/WHDLoad_Games/Games/
+if [ -d /home/pi/Amiga/dir/Work/WHDLoad_Games/ ]; then
+   mv  ~/Amiga/dir/Work/WHDLoad_Games/Games/* ~/Amiga/dir/Work/WHDLoad_Games/OCS/
+   #sudo rm -rf ~/Amiga/dir/Work/WHDLoad_Games/Games/
 fi
 # All the rest of files .. 
 # Cleanup OCR
-if [ -d /home/pi/Amiga/dir/Games/WHDLoad_Games/OCS/ ]; then
-cd ~/Amiga/dir/Games/WHDLoad_Games/OCS/
+if [ -d /home/pi/Amiga/dir/Work/WHDLoad_Games/OCS/ ]; then
+cd ~/Amiga/dir/Work/WHDLoad_Games/OCS/
 
 #Language
-find . -type d -name "*De" -exec sh -c 'mv -- "$0" ~/Amiga/dir/Games/WHDLoad_Games/De/' {} \;
+find . -type d -name "*De" -exec sh -c 'mv -- "$0" ~/Amiga/dir/Work/WHDLoad_Games/De/' {} \;
 find . -name "*De" -exec rm -r "{}" \;
-find . -type d -name "*Fr" -exec sh -c 'mv -- "$0" ~/Amiga/dir/Games/WHDLoad_Games/Fr/' {} \;
+find . -type d -name "*Fr" -exec sh -c 'mv -- "$0" ~/Amiga/dir/Work/WHDLoad_Games/Fr/' {} \;
 find . -name "*Fr" -exec rm -r "{}" \;
-find . -type d -name "*It" -exec sh -c 'mv -- "$0" ~/Amiga/dir/Games/WHDLoad_Games/It/' {} \;
+find . -type d -name "*It" -exec sh -c 'mv -- "$0" ~/Amiga/dir/Work/WHDLoad_Games/It/' {} \;
 find . -name "*It" -exec rm -r "{}" \;
-find . -type d -name "*Es" -exec sh -c 'mv -- "$0" ~/Amiga/dir/Games/WHDLoad_Games/Es/' {} \;
+find . -type d -name "*Es" -exec sh -c 'mv -- "$0" ~/Amiga/dir/Work/WHDLoad_Games/Es/' {} \;
 find . -name "*Es" -exec rm -r "{}" \;
-find . -type d -name "*Gr" -exec sh -c 'mv -- "$0" ~/Amiga/dir/Games/WHDLoad_Games/Gr/' {} \;
+find . -type d -name "*Gr" -exec sh -c 'mv -- "$0" ~/Amiga/dir/Work/WHDLoad_Games/Gr/' {} \;
 find . -name "*Gr" -exec rm -r "{}" \;
 
 # System
-find . -type d -name "*NTSC*" -exec sh -c 'mv -- "$0" ~/Amiga/dir/Games/WHDLoad_Games/NTSC/' {} \;
+find . -type d -name "*NTSC*" -exec sh -c 'mv -- "$0" ~/Amiga/dir/Work/WHDLoad_Games/NTSC/' {} \;
 find . -name "*NTSC*" -exec rm -r "{}" \;
-find . -type d -name "*AGA*" -exec sh -c 'mv -- "$0" ~/Amiga/dir/Games/WHDLoad_Games/AGA/' {} \;
+find . -type d -name "*AGA*" -exec sh -c 'mv -- "$0" ~/Amiga/dir/Work/WHDLoad_Games/AGA/' {} \;
 find . -name "*AGA*" -exec rm -r "{}" \;
-find . -type d -name "*CD32*" -exec sh -c 'mv -- "$0" ~/Amiga/dir/Games/WHDLoad_Games/CD32/' {} \;
+find . -type d -name "*CD32*" -exec sh -c 'mv -- "$0" ~/Amiga/dir/Work/WHDLoad_Games/CD32/' {} \;
 find . -name "*CD32*" -exec rm -r "{}" \;
-find . -type d -name "*CDTV*" -exec sh -c 'mv -- "$0" ~/Amiga/dir/Games/WHDLoad_Games/CDTV/' {} \;
+find . -type d -name "*CDTV*" -exec sh -c 'mv -- "$0" ~/Amiga/dir/Work/WHDLoad_Games/CDTV/' {} \;
 find . -name "*CDTV*" -exec rm -r "{}" \;
 
 fi
 
-cd /home/pi/Amiga/dir/Games/WHDLoad_Games/
+cd /home/pi/Amiga/dir/Work/WHDLoad_Games/
 find "$(pwd)" -name *.slave > ~/G.txt
 declare -a arr=( "Data" "data" "DATA"  )
 
@@ -159,4 +159,4 @@ sudo rm -rf ~/tmp1.txt
   	  echo " "
       
       
-      
+

7be06708d6c3f8b0f0255035239733f1ad8c390e
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2022-01-16 11:50:23 +0100

    Update check_status.sh

--- scripts/check_status.sh
+++ scripts/check_status.sh
@@ -1,3 +1,18 @@
+md5sum /etc/wpa_supplicant/wpa_supplicant.conf | cut -c -32 > /home/pi/.backup/default.md5
+md5sum /etc/wpa_supplicant/wpa_supplicant.conf | cut -c -32 > /home/pi/.backup/wpa.md5
+
+default="/home/pi/.backup/default.md5"
+act="/home/pi/.backup/wpa.md5"
+
+#if ! cmp --silent "$default" "$act"; then
+
+#echo " Is the same"
+#else
+#echo "WPA is set"
+
+#fi
+
+
 wget -q --spider http://google.com
 
  if [ $? -eq 0 ]; then

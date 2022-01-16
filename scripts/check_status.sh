b8a32cde09b86aee3f9ebc0577d3311e6b8d036e
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2022-01-16 14:19:54 +0100

    Update check_status.sh

--- scripts/check_status.sh
+++ scripts/check_status.sh
@@ -1,7 +1,10 @@
+#!/bin/bash
+# KickPi-OS
+# B.Titze 2022
 
 md5sum /etc/wpa_supplicant/wpa_supplicant.conf | cut -c -32 > /home/pi/.backup/wpa.md5
 
-default="/opt/KickPi-OS/config//default.md5"
+default="/opt/KickPi-OS/config/default.md5"
 act="/home/pi/.backup/wpa.md5"
 
 if ! cmp --silent "$default" "$act"; then
@@ -10,13 +13,12 @@ echo "WPA is set"
 else
 echo "WPA is default"
 
-whiptail --msgbox " Welcome to KickPi-OS V2, 
-    \n 
-    \n now lets setup your: 
+whiptail --msgbox " Welcome to KickPi-OS V2 
+    \n Now lets setup your:
     \n 
     \n - Password for Pi User
     \n - Wifi
-    \n - Language" 10 35 1
+    \n - Language" 20 40 1
     sudo -AE piwiz
     u
 

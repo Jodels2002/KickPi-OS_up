ec00bd687087890926e9e22b24f5f3066a6df5c1
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2022-04-14 15:25:23 +0200

    Update clean_home.sh

--- scripts/clean_home.sh
+++ scripts/clean_home.sh
@@ -10,8 +10,8 @@
       echo " Version V2.0 2020-2021 KickPi-OS "
       echo " No Rights Reserved.  "
       echo " "      
-      echo " "
       df $PWD | awk '/[0-9]%/{print $(NF-2)" free space"}'
+      echo " "
       echo "  ... delete unnecessary files  " 
       cd ~
       sudo find . -name "\._*" -type f -print0 | xargs -0 /bin/rm -f      
@@ -23,7 +23,6 @@
       echo "  ... delete mpeg files  " 
       sudo find . -name "*.mpg" -type f -print0 | xargs -0 /bin/rm -f
       echo " "      
-      echo " "
       echo "  ... delete unnecessary files finished:-)  " 
       df $PWD | awk '/[0-9]%/{print $(NF-2)" free space"}'
-
+      echo " "

da8cb02fb75418da9aff11cbe1642577fdfff621
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2022-04-20 07:14:23 +0200

    Update qemu.sh

--- scripts/qemu.sh
+++ scripts/qemu.sh
@@ -7,7 +7,7 @@ cd
 #sudo apt install -y virt-manager
 sudo apt install -y qemu-system-ppc
 
-
+# http://zero.eik.bme.hu/~balaton/qemu/amiga/
 qemu-system-ppc -machine sam460ex -rtc base=localtime \
   -drive if=none,id=cd,file=Sam460InstallCD-53.58.iso,format=raw \
   -device ide-cd,drive=cd,bus=ide.1

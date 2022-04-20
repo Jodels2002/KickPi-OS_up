09fe17d1b7b3c47a5527205b89d40e4b58c9acff
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2022-04-20 13:25:04 +0200

    Update qemu.sh

--- scripts/qemu.sh
+++ scripts/qemu.sh
@@ -13,5 +13,12 @@ sudo usermod -a -G libvirt pi
 qemu-system-ppc -machine Sam460 -sm501 -rtc base=localtime \
   -drive if=none,id=cd,file=Sam460InstallCD-53.58.iso,format=raw \
   -device ide-cd,drive=cd,bus=ide.1 \
+  
+  qemu-system-ppc -M sam460ex -m 2048 -rtc base=localtime \
+    -drive if=none,id=cd,file=Sam460InstallCD-53.58.iso,format=raw \
+    -device ide-cd,drive=cd,bus=ide.1 -drive if=none,id=hd,file=sam460.img,format=raw \ 
+    -device ide-hd,drive=hd,bus=ide.0 -boot order=c,menu=on \
+    -device rtl8139,netdev=mynet0 -netdev user,id=mynet0 \ 
+    -device es1370
 
 

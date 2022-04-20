b44a812f934d101a4afa918538228d3129552c27
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2022-04-20 14:01:06 +0200

    Update qemu.sh

--- scripts/qemu.sh
+++ scripts/qemu.sh
@@ -9,8 +9,7 @@ sudo apt install -y qemu-system-ppc
 sudo usermod -a -G libvirt pi
   
 qemu-img create -f qcow2 sam460.img 2G
-  
-qemu-img create -f qcow2 sam460.img 2G
+
   
 qemu-system-ppc -machine sam460ex -m 2048 -vga cirrus -rtc base=localtime -drive if=none,id=cd,file=Sam460InstallCD-53.58.iso,format=raw -device ide-cd,drive=cd,bus=ide.1 -drive if=none,id=hd,file=sam460.img,format=raw -device ide-hd,drive=hd,bus=ide.0 -boot order=c,menu=on -device rtl8139,netdev=mynet0 -netdev user,id=mynet0 -device es1370 
 

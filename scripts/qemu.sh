4bc5fc68eff5359f509716f1918a8557b0b7f9db
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2022-04-20 07:07:49 +0200

    Update qemu.sh

--- scripts/qemu.sh
+++ scripts/qemu.sh
@@ -3,8 +3,10 @@
 # B.Titze 2022
 cd
 
-sudo apt install -y qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils qemu*
-sudo apt install -y virt-manager
+#sudo apt install -y qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils 
+#sudo apt install -y virt-manager
+sudo apt install -y qemu-system-ppc
+
 
 qemu-system-ppc -machine sam460ex -rtc base=localtime \
   -drive if=none,id=cd,file=Sam460InstallCD-53.58.iso,format=raw \

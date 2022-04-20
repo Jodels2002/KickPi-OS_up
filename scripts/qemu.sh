5fa894d0a8deeb33f0960b09c5acb1191a3ec2ed
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2022-04-20 13:41:31 +0200

    Update qemu.sh

--- scripts/qemu.sh
+++ scripts/qemu.sh
@@ -6,16 +6,18 @@ cd
 sudo apt install -y qemu-kvm libvirt-daemon-system libvirt-bin libvirt-clients bridge-utils 
 sudo apt install -y virt-manager
 sudo apt install -y qemu-system-ppc
-sudo adduser pi libvirtd
 sudo usermod -a -G libvirt pi
-
-# http://zero.eik.bme.hu/~balaton/qemu/amiga/
-qemu-system-ppc -machine Sam460 -sm501 -rtc base=localtime \
-  -drive if=none,id=cd,file=Sam460InstallCD-53.58.iso,format=raw \
-  -device ide-cd,drive=cd,bus=ide.1 \
   
-  qemu-img create -f qcow2 sam460.img 2G
+qemu-img create -f qcow2 sam460.img 2G
   
-qemu-system-ppc -M sam460ex -m 2048 -rtc base=localtime -drive if=none,id=cd,file=Sam460InstallCD-53.58.iso,format=raw -device ide-cd,drive=cd,bus=ide.1 -drive if=none,id=hd,file=sam460.img,format=raw -device ide-hd,drive=hd,bus=ide.0 -boot order=c,menu=on -device rtl8139,netdev=mynet0 -netdev user,id=mynet0 -device es1370
+qemu-system-ppc -M sam460ex -m 2048 -rtc base=localtime \ 
+-drive if=none,id=cd,file=Sam460InstallCD-53.58.iso,format=raw \
+-device ide-cd,drive=cd,bus=ide.1 \
+-drive if=none,id=hd,file=sam460.img,format=raw \
+-device ide-hd,drive=hd,bus=ide.0 \ 
+-boot order=c,menu=on \ 
+-device rtl8139,netdev=mynet0 \ 
+-netdev user,id=mynet0 \ 
+-device es1370 \
 
 

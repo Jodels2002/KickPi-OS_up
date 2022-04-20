8df2663608e2ff7615ebd9f0704496683612611c
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2022-04-20 13:33:52 +0200

    Update qemu.sh

--- scripts/qemu.sh
+++ scripts/qemu.sh
@@ -16,11 +16,6 @@ qemu-system-ppc -machine Sam460 -sm501 -rtc base=localtime \
   
   qemu-img create -f qcow2 sam460.img 2G
   
-  qemu-system-ppc -M sam460ex -m 2048 -rtc base=localtime \
-    -drive if=none,id=cd,file=Sam460InstallCD-53.58.iso,format=raw \
-    -device ide-cd,drive=cd,bus=ide.1 -drive if=none,id=hd,file=sam460.img,format=raw \ 
-    -device ide-hd,drive=hd,bus=ide.0 -boot order=c,menu=on \
-    -device rtl8139,netdev=mynet0 -netdev user,id=mynet0 \ 
-    -device es1370
+qemu-system-ppc -M sam460ex -m 2048 -rtc base=localtime -drive if=none,id=cd,file=Sam460InstallCD-53.58.iso,format=raw -device ide-cd,drive=cd,bus=ide.1 -drive if=none,id=hd,file=sam460.img,format=raw -device ide-hd,drive=hd,bus=ide.0 -boot order=c,menu=on -device rtl8139,netdev=mynet0 -netdev user,id=mynet0 -device es1370
 
 

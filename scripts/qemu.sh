7737520624d7ca6314992d15f57e7c039fd47df5
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2022-04-20 10:37:44 +0200

    Update qemu.sh

--- scripts/qemu.sh
+++ scripts/qemu.sh
@@ -14,3 +14,7 @@ qemu-system-ppc -machine sam460ex -rtc base=localtime \
   -drive if=none,id=cd,file=Sam460InstallCD-53.58.iso,format=raw \
   -device ide-cd,drive=cd,bus=ide.1 \
   -vga cirrus
+  
+  qemu-system-ppc -machine sam460ex -rtc base=localtime \
+  -drive if=none,id=cd,file=morphos-3.16.iso,format=raw \
+  -device ide-cd,drive=cd,bus=ide.1

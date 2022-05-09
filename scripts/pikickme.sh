b9ade560cf3018063e6d936d8bb1d3e09ea0c85b
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2022-05-09 07:56:47 +0100

    Update pikickme.sh

--- scripts/pikickme.sh
+++ scripts/pikickme.sh
@@ -9,8 +9,7 @@
       GREEN='\033[1;32m'
       RED='\033[1;31m'
       GREY='\033[1;30m'
-      sudo mkdir /opt/KickPi-OS/Backup/
-       sudo mkdir /opt/Backup/
+     
       sudo chmod -R 777 /home/$USER/KickPi-OS
       if [ ! -f /home/pi/OLED.txt ]; then
       

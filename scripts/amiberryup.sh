b81b87d8b1adc5c1fcdfc34efeae9ed6add10d1f
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2022-07-02 06:06:48 +0200

    Update amiberryup.sh

--- scripts/amiberryup.sh
+++ scripts/amiberryup.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 # Install KickPi-OS
 # B.Titze 2021
-  sudo apt-get -y install libsdl2-2.0-0 libsdl2-ttf-2.0-0 libsdl2-image-2.0-0 flac mpg123 libmpeg2-4
-  sudo apt-get -y install libsdl2-dev libsdl2-ttf-dev libsdl2-image-dev libflac-dev libmpg123-dev libpng-dev libmpeg2-4-dev libraspberrypi-dev
-
+sudo apt-get install -y libraspberrypi-dev
 
 Update_Amiberry.sh
   
   
 
- cd
- sudo rm -rf amiberry
+ cd /home/pi/Amiga/Install/
+
  
  git clone -b dev https://github.com/midwan/amiberry
       # git clone https://github.com/midwan/amiberry
@@ -20,7 +18,7 @@ if [ "$(getconf LONG_BIT)" == "64" ]; then
       clear
       toilet "KickPi-OS" --metal
       echo ""
-      echo "Compiling Amiberry 64bit Dispmanx (DEV)"
+      echo "Raspberry Pi 4 OS 64 bit SDL-Dispmanx"
       echo ""
     
       #make -j4 PLATFORM=rpi4-64-sdl2
@@ -30,16 +28,51 @@ if [ "$(getconf LONG_BIT)" == "64" ]; then
       echo "Raspberry Pi OS 32 bit is running... "
       echo ""
       toilet "KickPi-OS" --metal
+      REVCODE=$(sudo cat /proc/cpuinfo | grep 'Revision' | awk '{print $3}' | sed 's/^ *//g' | sed 's/ *$//g')
+      
+	if [ "$REVCODE" = "a02082" ]; then
+    	PIMODEL="Raspberry Pi 3 Model B, 1 GB RAM"
+		echo "$PIMODEL ($REVCODE)"
+		echo ""
+      		
+     		make -j2 PLATFORM=rpi3
 
-	      echo ""
-	      echo "Raspberry Pi 4 SDL 32bit"
+	fi
+	if [ "$REVCODE" = "a020d3" ]; then
+	    PIMODEL="Raspberry Pi 3 Model B Plus, 1 GB RAM"
+		echo ""
+		echo "$PIMODEL ($REVCODE)"
 	     
-	      make -j4 PLATFORM=rpi4
+	      make -j2 PLATFORM=rpi3
 
+
+	fi
+	if [ "$REVCODE" = "a22082" ]; then
+	    PIMODEL="Raspberry Pi 3 Model B, 2 GB RAM"
+		echo "$PIMODEL ($REVCODE)"
+		echo ""
+	     
+	      make -j2 PLATFORM=rpi3
+	fi
+	      
+fi  
+
+if [ "$REVCODE" = "9000C1" ]; then
+    PIMODEL="Raspberry Pi Zero W"
+	echo "$PIMODEL ($REVCODE)"
+	sudo raspi-config nonint is_pi
+        make PLATFORM=rpi1
 
 fi
 
-sudo chmod -R 777 /home/$USER/amiberry/amiberry
-cp -rf /home/$USER/amiberry/*  /home/pi/Amiga/
-cp -rf /home/$USER/amiberry/amiberry  /home/pi/Amiga/amiberry_sdl
+
+sudo chmod -R 777 /home/$USER/amiberry
+
+cp -rf /home/$USER/amiberry/amiberry  /home/$USER/amiberry/amiberry_old
+cp -rf /home/pi/Amiga/Install/amiberry/amiberry  /home/$USER/Amiga/ 
+cp -rf /home/pi/Amiga/Install/amiberry/amiberry  /home/$USER/Amiga/amiberry_dev
+cp -rf  /home/pi/Amiga/amiberry_dev /opt/Backup
+cp -rf /home/pi/Amiga/amiberry  /home/pi/Amiga/amiberry_old
+cp -rf /home/$USER/amiberry/amiberry  /home/pi/Amiga/amiberry_dmx
+sudo rm -rf /home/pi/Amiga/Install/amiberry
 

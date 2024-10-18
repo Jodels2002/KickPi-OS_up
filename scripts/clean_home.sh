dec5c803a0432d618423db4570b629916fc847b9
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2024-10-18 11:52:36 +0200

    Update clean_home.sh

--- scripts/clean_home.sh
+++ scripts/clean_home.sh
@@ -17,8 +17,17 @@
       sudo find . -name "\._*" -type f -print0 | xargs -0 /bin/rm -f      
       echo "  ... delete Spotlight files  " 
       sudo find . -name "_UAEFSDB.*" -type f -print0 | xargs -0 /bin/rm -f
-      sudo find . -name "*.mp4" -type f -print0 | xargs -0 /bin/rm -f
-      echo "  ... delete mp4 files  " 
+      sudo find . -name ".DS_Store" -type f -print0 | xargs -0 /bin/rm -f
+      echo "  ... delete mpeg files  " 
+      sudo find . -name "*.mpg" -type f -print0 | xargs -0 /bin/rm -f
+      echo " "      
+      echo "  ... delete unnecessary files finished:-)  " 
+      df $PWD | awk '/[0-9]%/{print $(NF-2)" free space"}'
+      echo " "
+      cd /opt
+      sudo find . -name "\._*" -type f -print0 | xargs -0 /bin/rm -f      
+      echo "  ... delete Spotlight files  " 
+      sudo find . -name "_UAEFSDB.*" -type f -print0 | xargs -0 /bin/rm -f
       sudo find . -name ".DS_Store" -type f -print0 | xargs -0 /bin/rm -f
       echo "  ... delete mpeg files  " 
       sudo find . -name "*.mpg" -type f -print0 | xargs -0 /bin/rm -f

efede4f2cde2697efe70cff07b076c8309baf5e4
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2025-04-02 11:27:54 +0200

    Update kickmenu.sh

--- scripts/kickmenu.sh
+++ scripts/kickmenu.sh
@@ -73,7 +73,8 @@ case $CHOICE in
          l)
             #Cool-Retro-Term
             sudo apt install -y build-essential qmlscene qt5-qmake qtbase5-dev qtdeclarative5-dev qml-module-qtquick-controls2 qml-module-qtgraphicaleffects qml-module-qtquick-dialogs qml-module-qtquick-localstorage qml-module-qtquick-window2 qml-module-qt-labs-settings qml-module-qt-labs-folderlistmodel qml-module-qt-labs-platform qtquickcontrols2-5-dev qml-module-qtquick-layouts
-            git clone --recursive https://github.com/Swordfish90/cool-retro-term.git
+            sudo apt install -y build-essential qmlscene qt5-qmake qtdeclarative5-dev qml-module-qtquick-controls2 qml-module-qtgraphicaleffects qml-module-qtquick-dialogs qml-module-qtquick-localstorage qml-module-qtquick-window2 qml-module-qt-labs-settings qml-module-qt-labs-folderlistmodel qtdeclarative5-dev qtquickcontrols2-5-dev qml-module-qt-labs-platform
+	    git clone --recursive https://github.com/Swordfish90/cool-retro-term.git
 
             # Build it
               cd cool-retro-term

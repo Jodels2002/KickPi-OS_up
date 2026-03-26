28d8221f7f476e66ac85bc01c7451d76bba42d04
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2026-03-26 08:15:31 +0100

    Update kickmenu.sh

--- scripts/kickmenu.sh
+++ scripts/kickmenu.sh
@@ -52,7 +52,7 @@ update_amiberry() {
 
     cd "$HOME/amiberry"
     cmake -B build
-    cmake --build build -j4
+    cmake --build build 
 
     sudo mkdir -p "$BACKUP_DIR"
 
@@ -84,7 +84,7 @@ update_amiberry_dev() {
     git clone https://github.com/BlitterStudio/amiberry-lite "$HOME/amiberry-lite"
     cd "$HOME/amiberry-lite"
 
-    cmake -B build && cmake --build build -j4
+    cmake -B build && cmake --build build 
 
     sudo cp build/amiberry-lite "$HOME/Amiga/amiberry_dev"
     sudo cp -r data "$HOME/Amiga"
